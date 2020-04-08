3f807d54ce91a004fbb3a447ff6a8ffddb255674
chihirotanaka <senno330@gmail.com>
2020-04-08 04:44:38 +0000

    [finish]

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,13 +6,14 @@ class FavoritesController < ApplicationController
 			favorite =current_user.favorites.new(book_id: book.id)
 			favorite.save
 		end
-		redirect_to books_path
+		# book indexから行った時
+		redirect_to request.referrer
 	end
 
 	def destroy
 		book = Book.find(params[:book_id])
 		favorite =current_user.favorites.find_by(book_id: book.id)
 		favorite.destroy
-		redirect_to books_path
+		redirect_to request.referrer
 	end
 end
